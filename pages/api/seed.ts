c96e58e6bdd0a07c69b3b19c8b6a16074988b5f4
Onurkan Bakırcı <onurkan_bkrc@hotmail.com>
2024-11-03 21:50:30 +0300

    fix: serverless timout increased

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,10 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import getTrendingRepos from 'scripts/github-setup';
 
-export const config = {
-  maxDuration: 60
-};
-
 async function handler(_req: NextApiRequest, res: NextApiResponse) {
   const message = await getTrendingRepos();
 
