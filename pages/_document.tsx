c15dc177b55f85b44d38720ce4b3b55f0e0e6f27
Onurkan Bakırcı <onurkan_bkrc@hotmail.com>
2024-11-04 12:27:42 +0300

    fix: err fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { Analytics } from "@vercel/analytics/react"
-import { AxiomWebVitals } from "next-axiom"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -11,8 +9,6 @@ export default function Document() {
         <Main />
         <NextScript />
         <Analytics />
-        <SpeedInsights />
-        <AxiomWebVitals />
       </body>
     </Html>
   );
