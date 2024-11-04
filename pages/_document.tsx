ce96a6502d90d6303023d2bc269d162bf834da89
Onurkan Bakırcı <onurkan_bkrc@hotmail.com>
2024-11-04 12:24:28 +0300

    fix: analytics added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { Analytics } from "@vercel/analytics/react"
+import { AxiomWebVitals } from "next-axiom"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -9,6 +11,8 @@ export default function Document() {
         <Main />
         <NextScript />
         <Analytics />
+        <SpeedInsights />
+        <AxiomWebVitals />
       </body>
     </Html>
   );
