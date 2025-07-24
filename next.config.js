751cb94281d4ff407f505596261a54f9889b3d66
Onurkan Bakırcı <onurkan_bkrc@hotmail.com>
2025-07-24 12:49:55 +0300

    chore: update next.config.js for standalone output and add Netlify plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   swcMinify: true,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         hostname: '**'
       }
     ]
+  },
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true
   },
   target: 'serverless'
 };
